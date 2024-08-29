f10f63be4b229b86fd3b2cfcc468ae7a83c7f476
Vlad <eternaleapfrompit@gmail.com>
2024-08-29 07:29:58 +0700

    fixed docker connection to rabbitmq through adding link

--- RouteFinder.RabbitMq/DependencyInjection.cs
+++ RouteFinder.RabbitMq/DependencyInjection.cs
@@ -8,13 +8,16 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddRabbitMq(this IServiceCollection services)
     {
+        Thread.Sleep(3*60*100);
+        
         return services.AddMassTransit(x =>
         {
             x.AddConsumer<StringConsumer>();
             
+            // have to connect to 5672 as it's default
             x.UsingRabbitMq((context, cfg) =>
             {
-                cfg.Host("localhost", "/", h =>
+                cfg.Host(new Uri("amqp://guest:guest@rabbitmq/"), "/", h =>
                 {
                     h.Username("guest");
                     h.Password("guest");
